0bb3a08ab58f3634739853e93d6778ebf8b9e737
letyang23 <yangletian0309@gmail.com>
2024-07-21 16:09:31 -0700

    Solved 3Sum

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -4,6 +4,7 @@
 # [15] 3Sum
 #
 
+
 # @lc code=start
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -14,12 +15,27 @@ class Solution:
         nums.sort()
 
         for i, num in enumerate(nums):
-            
+
             # skip the duplicate
-            if i > 0 and num = nums[i-1]:
+            if i > 0 and num == nums[i - 1]:
                 continue
-            
+
+            # easier way to do twoSum when the list is sorted
+            # use left and right Two-Pointer
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                threeSum = num + nums[left] + nums[right]
+                if threeSum > 0:
+                    right -= 1
+                elif threeSum < 0:
+                    left += 1
+                else:
+                    result.append([num, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
 
         return result
-# @lc code=end
 
+
+# @lc code=end
