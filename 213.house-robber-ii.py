1f61b59931c0e0e7dd74c5ea0c5eac83b639af80
letyang23-windows <yangletian0309@gmail.com>
2024-10-01 17:44:35 -0700

    modify

--- 213.house-robber-ii.py
+++ 213.house-robber-ii.py
@@ -7,16 +7,19 @@
 # @lc code=start
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
 
-    def helper(self, nums):
-        rob1, rob2 = 0, 0
+        # House Robber 1 solution
+        def helper(nums):
+            rob1, rob2 = 0, 0
+            
+            for n in nums:
+                temp = max(rob1 + n, rob2)
+                rob1 = rob2
+                rob2 = temp
+            return rob2
         
-        for n in nums:
-            temp = max(rob1 + n, rob2)
-            rob1 = rob2
-            rob2 = temp
-        return rob2
+        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
+
 
 # @lc code=end
 
