1e282a6a8c38365b1224a7a50ee7d990e9b5d36f
letyang23 <yangletian0309@gmail.com>
2024-10-05 18:57:45 -0700

    Graph start (need comments)

--- 133.clone-graph.py
+++ 133.clone-graph.py
@@ -16,6 +16,20 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            return copy
+
+        return dfs(node) if node else None
+
+
 # @lc code=end
 
