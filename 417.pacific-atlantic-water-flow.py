5e1de58fa0d42c09d48ce1dfd293215d6b5b3cdb
letyang23 <yangletian0309@gmail.com>
2024-10-07 00:05:47 -0700

    add some comments for 417

--- 417.pacific-atlantic-water-flow.py
+++ 417.pacific-atlantic-water-flow.py
@@ -8,10 +8,20 @@
 # @lc code=start
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
         ROWS, COLS = len(heights), len(heights[0])
+
+        # pac = set of cell that can reach pacific
+        # atl = set of cell that can reach atlantic
         pac, atl = set(), set()
 
+        # dfs algorithm to exploring neighboring cells
+        # (r, c) is the coordinates
+        # visit is the set atl/pac
+        # prevHeight is the Height of current cell (to be compared with neighboring cell)
         def dfs(r, c, visit, prevHeight):
+
+            # conditions to stop exploring
             if (
                 (r, c) in visit
                 or r < 0
@@ -21,16 +31,20 @@ class Solution:
                 or heights[r][c] < prevHeight
             ):
                 return
+            
+            # cell is valid and add to the set
             visit.add((r, c))
+
+            # exploring up, down, left, right cells
             dfs(r + 1, c, visit, heights[r][c])
             dfs(r - 1, c, visit, heights[r][c])
             dfs(r, c + 1, visit, heights[r][c])
             dfs(r, c - 1, visit, heights[r][c])
 
+        # exploring the edge (reach them = reach the ocean)
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
             dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
-
         for r in range(ROWS):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS - 1, atl, heights[r][COLS - 1])
