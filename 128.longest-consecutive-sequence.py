5ffcadb7e3ff7fbdc09f40f4ef5a46cad984a7e9
letyang23 <yangletian0309@gmail.com>
2024-10-08 16:16:29 -0700

    add comments

---
+++ 128.longest-consecutive-sequence.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=128 lang=python3
+#
+# [128] Longest Consecutive Sequence
+#
+
+# @lc code=start
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # Does it have a left neighbor?
+        # if it doesn't, it is a start of a sequence
+
+        # convert into a set
+# @lc code=end
+
