7e93d2a1d9512bf5523975483a23253071f01f3f
letyang23 <yangletian0309@gmail.com>
2024-10-09 14:47:20 -0700

    Create 269.alien-dictionary.py

---
+++ 269.alien-dictionary.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=269 lang=python3
+#
+# [269] Alien Dictionary
+#
+
+# @lc code=start
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        
+# @lc code=end
+
