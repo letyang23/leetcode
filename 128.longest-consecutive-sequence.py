d80bf3c00a954bc4f9f3cd1d657097b917997ab0
letyang23 <yangletian0309@gmail.com>
2024-10-08 23:45:55 -0700

    add comments for 128

--- 128.longest-consecutive-sequence.py
+++ 128.longest-consecutive-sequence.py
@@ -4,6 +4,7 @@
 # [128] Longest Consecutive Sequence
 #
 
+
 # @lc code=start
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
@@ -11,17 +12,21 @@ class Solution:
         # if it doesn't, it is a start of a sequence
 
         # convert into a set
+
+        # why make it a set?
+        # "In python, set is implemented as a hash table.
+        # So you can expect to lookup/insert/delete in O(1) average."
         numSet = set(nums)
         longest = 0
 
         for n in nums:
             # check if its the start of a sequence
-            if (n-1) not in numSet:
+            if (n - 1) not in nums:
                 length = 0
-                while (n+length) in numSet:
+                while (n + length) in nums:
                     length += 1
                 longest = max(length, longest)
         return longest
 
-# @lc code=end
 
+# @lc code=end
