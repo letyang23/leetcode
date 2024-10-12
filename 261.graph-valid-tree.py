2300c3266f6c72f36cdf050f40c25b03d2ced90b
letyang23 <yangletian0309@gmail.com>
2024-10-12 02:29:19 -0700

    Update 261.graph-valid-tree.py

--- 261.graph-valid-tree.py
+++ 261.graph-valid-tree.py
@@ -4,9 +4,33 @@
 # [261] Graph Valid Tree
 #
 
+
 # @lc code=start
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
-# @lc code=end
+        if not n:
+            return True
 
+        adj = {i: [] for i in range(n)}  # Create a hashset
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        visit = set()  # visit set
+
+        def dfs(i, prev):  # record prev for loop detection
+            if i in visit:
+                return False
+
+            visit.add(i)
+            for j in adj[i]:
+                if j == prev:
+                    continue
+                if not dfs(j, i):
+                    return False
+            return True
+
+        return dfs(0, -1) and n == len(visit)
+
+
+# @lc code=end
