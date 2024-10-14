7de05bdef4355fedc561394875902c6c4c3cca07
letyang23 <yangletian0309@gmail.com>
2024-10-13 18:29:05 -0700

    Create 323.number-of-connected-components-in-an-undirected-graph.py

---
+++ 323.number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=323 lang=python3
+#
+# [323] Number of Connected Components in an Undirected Graph
+#
+
+# @lc code=start
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+# @lc code=end
+
