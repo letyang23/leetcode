01032e5a9ace692601a9c3482f2fff161b5b005f
letyang23 <yangletian0309@gmail.com>
2024-10-14 23:41:08 -0700

    Update 57.insert-interval.py

--- 57.insert-interval.py
+++ 57.insert-interval.py
@@ -10,11 +10,16 @@ class Solution:
         result = []
 
         for i in range(len(intervals)):
+            # Case 1: No overlap, and newInterval goes before the current interval
             if newInterval[1] < intervals[i][0]:
                 result.append(newInterval)
                 return result + intervals[i:]
+            
+            # Case 2: No overlap, and newInterval goes after the current interval
             elif newInterval[0] > intervals[i][1]:
                 result.append(intervals[i])
+
+            # Case 3: Overlapping intervals - merge to newInterval
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         
