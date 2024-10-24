6ee004226d2677e4b394ef656e40293b68e2c20d
letyang23 <yangletian0309@gmail.com>
2024-10-23 23:31:02 -0700

    Create 380.insert-delete-get-random-o-1.py

---
+++ 380.insert-delete-get-random-o-1.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=380 lang=python3
+#
+# [380] Insert Delete GetRandom O(1)
+#
+
+# @lc code=start
+class RandomizedSet:
+
+    def __init__(self):
+        self.map = {}
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        if val in self.map:
+            return False
+        self.map[val] = len(self.list)
+        self.list.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.map:
+            return False
+        # delete element in the list and replace it with the last element in the list
+        index = self.map[val]
+        lastVal = self.list[-1]
+        self.list[index] = lastVal
+        self.list.pop()
+        self.map[lastVal] = index
+
+        del self.map[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.list)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+# @lc code=end
+
