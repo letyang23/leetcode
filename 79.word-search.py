f152a9ea831d043acbd6bf16807ce4f5ecdc4916
letyang23 <yangletian0309@gmail.com>
2024-11-11 12:29:24 -0800

    Create 79.word-search.py

---
+++ 79.word-search.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=79 lang=python3
+#
+# [79] Word Search
+#
+
+# @lc code=start
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+# @lc code=end
+
