3ee6b99eb7d230e3bbeb7307227f191713680aa6
letyang23 <yangletian0309@gmail.com>
2025-01-05 17:22:23 -0800

    Import from PyCharm

---
+++ leetcode/editor/en/[5]Longest Palindromic Substring.py
@@ -0,0 +1,9 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+# leetcode submit region end(Prohibit modification and deletion)
+
+solution = Solution()
+print(solution.longestPalindrome("babad"))
