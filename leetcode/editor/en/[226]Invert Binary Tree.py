11349dfc4d9662ed74af1fac64550aad537fb907
letyang23 <yangletian0309@gmail.com>
2025-01-11 15:21:43 -0800

    Invert Binary Tree

--- leetcode/editor/en/[226]Invert Binary Tree.py
+++ leetcode/editor/en/[226]Invert Binary Tree.py
@@ -15,7 +15,16 @@ class TreeNode:
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        
+        # DFS Recursion
+        if not root:
+            return None
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
 # leetcode submit region end(Prohibit modification and deletion)
 
 # Helper Function
@@ -37,28 +46,60 @@ def to_binary_tree(items):
             q.append(node.right)
     return root
 
+# Helper function to display the binary tree
+def display(root):
+    lines, *_ = _display_aux(root)
+    for line in lines:
+        print(line)
 
-# Helper function to print all nodes in level-order (Breadth-First Search)
-def print_tree(root):
-    if not root:
-        return "Tree is empty"
+def _display_aux(self):
+    """Returns list of strings, width, height, and horizontal coordinate of the root."""
+    # No child.
+    if self.right is None and self.left is None:
+        line = '%s' % self.val
+        width = len(line)
+        height = 1
+        middle = width // 2
+        return [line], width, height, middle
 
-    result = []
-    queue = [root]
-    while queue:
-        node = queue.pop(0)
-        if node:
-            result.append(node.val)
-            queue.append(node.left)
-            queue.append(node.right)
-        else:
-            result.append(None)  # To represent null nodes for a complete tree view
-    # Remove trailing None values for a cleaner output
-    while result and result[-1] is None:
-        result.pop()
-    return result
+    # Only left child.
+    if self.right is None:
+        lines, n, p, x = _display_aux(self.left)
+        s = '%s' % self.val
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+        second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+        shifted_lines = [line + u * ' ' for line in lines]
+        return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+    # Only right child.
+    if self.left is None:
+        lines, n, p, x = _display_aux(self.right)
+        s = '%s' % self.val
+        u = len(s)
+        first_line = s + x * '_' + (n - x) * ' '
+        second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+        shifted_lines = [u * ' ' + line for line in lines]
+        return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+    # Two children.
+    left, n, p, x = _display_aux(self.left)
+    right, m, q, y = _display_aux(self.right)
+    s = '%s' % self.val
+    u = len(s)
+    first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+    second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+    if p < q:
+        left += [n * ' '] * (q - p)
+    elif q < p:
+        right += [m * ' '] * (p - q)
+    zipped_lines = zip(left, right)
+    lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+    return lines, n + m + u, max(p, q) + 2, n + u // 2
+
 
 # Test Case
 root = to_binary_tree([4,2,7,1,3,6,9])
+display(root)
 solution = Solution()
-print_tree(solution.invertTree(root))
+display(solution.invertTree(root))
