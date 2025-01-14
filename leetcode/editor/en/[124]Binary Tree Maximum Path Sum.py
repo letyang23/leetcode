6f5615846845b18e56e0f439303ef9e476ce77c0
letyang23 <yangletian0309@gmail.com>
2025-01-13 20:45:22 -0800

    stressin

---
+++ leetcode/editor/en/[124]Binary Tree Maximum Path Sum.py
@@ -0,0 +1,63 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        result = [root.val]     # use list as variable could avoid using nonlocal
+
+        # return max path sum without split
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            # compute max path sum WITH split
+            result[0] = max(result[0], root.val + leftMax + rightMax)
+
+            return root.val + max(leftMax, rightMax)
+
+        dfs(root)
+        return result[0]
+# leetcode submit region end(Prohibit modification and deletion)
+
+# Helper Function
+def to_binary_tree(items):
+    if not items:
+        return None
+
+    it = iter(items)
+    root = TreeNode(next(it))
+    q = [root]
+    for node in q:
+        val = next(it, None)
+        if val is not None:
+            node.left = TreeNode(val)
+            q.append(node.left)
+        val = next(it, None)
+        if val is not None:
+            node.right = TreeNode(val)
+            q.append(node.right)
+    return root
+
+
+# Test Case
+node = [-10,9,20,None,None,15,7]
+root = to_binary_tree(node)
+solution = Solution()
+print(solution.maxPathSum(root))
