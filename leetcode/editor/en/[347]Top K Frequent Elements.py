4b38f0f6acad7b9cacb89484eadbde13cd7d56f7
letyang23 <yangletian0309@gmail.com>
2025-02-04 18:35:06 -0800

    update blind 75

---
+++ leetcode/editor/en/[347]Top K Frequent Elements.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
+
+solution = Solution()
