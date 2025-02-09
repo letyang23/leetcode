d09561e443d8d6d3242b4fc7a6d295fa0f0392f6
letyang23 <yangletian0309@gmail.com>
2025-02-08 18:06:03 -0800

    Update [1]Two Sum.py

--- leetcode/editor/en/[1]Two Sum.py
+++ leetcode/editor/en/[1]Two Sum.py
@@ -1,8 +1,17 @@
+from typing import List
+
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        numsDict = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in numsDict:
+                return [numsDict[diff], i]
+            numsDict[nums[i]] = i
 # leetcode submit region end(Prohibit modification and deletion)
 
 solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))
