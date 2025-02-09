66a0650eb65b23c9d5c251b58ac39e5fdd298a86
letyang23 <yangletian0309@gmail.com>
2025-02-08 17:51:41 -0800

    Finish Blind 75

---
+++ leetcode/editor/en/[1]Two Sum.py
@@ -0,0 +1,8 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+# leetcode submit region end(Prohibit modification and deletion)
+
+solution = Solution()
---
+++ leetcode/editor/en/[236]Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,15 @@
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+# leetcode submit region end(Prohibit modification and deletion)
+
+solution = Solution()
